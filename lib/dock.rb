c2333ffa445dde478d33616f044db9bcf6a3ce75
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-24 17:45:48 +0100

    Implement bike docking feature

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,5 +1,12 @@
 class Dock
-	def release_bike
+  
+  attr_reader :docked_bike
+
+	def release_bike 
 		Bike.new
 	end
+
+  def initialize(bike)
+    @docked_bike = bike
+  end
 end
--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -1,21 +1,28 @@
 require './lib/dock.rb'
+require './lib/bike.rb'
+
 
 describe Dock do
 
 	before(:each) do
-		@dock = Dock.new
+		@dock = Dock.new(Bike.new)
 	end
 
-	it 'is not nil' do
-		expect(@dock).not_to be nil
+	it 'expects #new to have an argument' do
+		expect{Dock.new(Bike.new)}.to_not raise_error
 	end
 
 	it 'respond to release_bike' do
 		expect(@dock).to respond_to(:release_bike)
 	end
 
-	it 'get\'s a bike and checks if it works' do
+	it 'returns a bike and checks if it works' do
 		expect(@dock.release_bike.class).to eq Bike
 		expect(@dock.release_bike.working?).to eq true
 	end
+
+	it '#docked_bike returns instance variable @docked_bike' do
+		expect(@dock.docked_bike.class).to eq Bike
+	end
+  
 end
