ad828f1afc89c0d2c291913541f377be5e9459e0
srp2930 <s.rimingtonpounder@gmail.com>
2016-05-25 16:02:17 +0100

    nested tests under method names

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -11,29 +11,34 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'returns empty if no bike has been docked' do
-    expect(subject.bikes).to eq([])
+  describe '#bikes' do
+    it 'returns empty if no bike has been docked' do
+      expect(subject.bikes).to eq([])
+    end
   end
 
-  it 'returns the bike if bike has been docked' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bikes).to eq([bike])
+  describe '#dock' do
+    it 'returns the bike if bike has been docked' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bikes).to eq([bike])
+    end
+
+    it 'should not accept bike if full' do
+      20.times { subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error("Dock is full")
+    end
   end
 
-  it 'should not give out infinite bikes' do
-    expect { subject.release_bike }.to raise_error("Bike not available")
-  end
+  describe '#release_bike' do
+    it 'should not give out infinite bikes' do
+      expect { subject.release_bike }.to raise_error("Bike not available")
+    end
 
-  it 'should not accept bike if full' do
-    20.times { subject.dock(Bike.new) }
-    expect { subject.dock(Bike.new) }.to raise_error("Dock is full")
+    it 'should return bike if one is docked' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq(bike)
+    end
   end
-
-  it 'should return bike if one is docked' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.release_bike).to eq(bike)
-  end
-
 end
