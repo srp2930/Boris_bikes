c4daffd59e837a1c3c501f91cdf68a907438dff0
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-05-26 14:01:00 +0100

    separate full condition into a predicate method

--- features.rb
+++ features.rb
@@ -1,6 +1,6 @@
 require './lib/docking_station.rb'
 require './lib/bike.rb'
 d = DockingStation.new
-b = Bike.new
+b = [Bike.new]
 
 d.dock(b)
--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,7 +5,6 @@ class DockingStation
 
   def initialize
     @docked_bikes = []
-    @full = false
     @capacity = 20
   end
 
@@ -13,22 +12,19 @@ class DockingStation
 		if @docked_bikes == []
 			raise "No bikes available."
 		else 
-      released_bike = @docked_bikes.pop
-      @full = false
-      released_bike
+      @docked_bikes.pop
 		end
 	end
 	
 	def dock(bikes)
     bikes = [bikes].flatten    
 
-    if @full
+    if full?
       raise "Station full."
     elsif bikes == []
       raise "No bikes in the input array"
     elsif n_of_input_bikes_small_enough?(bikes)
        (@docked_bikes << bikes).flatten!
-       @full = true if @docked_bikes.length == @capacity
     else   
       raise 'Not enough capacity in dock to accommodate bikes in array'
     end
@@ -43,6 +39,10 @@ class DockingStation
 	end
 
 private
+
+  def full?
+    @docked_bikes.length >= @capacity
+  end
 
   def n_of_input_bikes_small_enough?(bikes)
     (@docked_bikes.length + bikes.length) <= @capacity
